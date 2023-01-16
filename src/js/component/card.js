2b2016436f516bd3a4cd59a590de8adbce0c66ce
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-16 20:57:07 +0000

    https://4geeks.com/syllabus/latam-ft-uy-2/project/starwars-blog-reading-list

---
+++ src/js/component/card.js
@@ -0,0 +1,64 @@
+import React from "react";
+
+const Card = () => {
+    return ( 
+        <div className="card-group row">
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                    <h5 className="card-title">Character</h5>
+                    <p className="card-text">Description</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-primary">Learn more!</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                    <h5 className="card-title">Character</h5>
+                    <p className="card-text">Description</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-primary">Learn more!</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                    <h5 className="card-title">Character</h5>
+                    <p className="card-text">Description</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-primary">Learn more!</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                    <h5 className="card-title">Character</h5>
+                    <p className="card-text">Description</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-primary">Learn more!</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                    <h5 className="card-title">Character</h5>
+                    <p className="card-text">Description</p>
+                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                    <a href="#" class="btn btn-primary">Learn more!</a>
+                </div>
+            </div>
+            <div className="card">
+                <img src="..." className="card-img-top" alt="image"/>
+                <div className="card-body">
+                <h5 className="card-title">Character</h5>
+                <p className="card-text">Description</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                <a href="#" class="btn btn-primary">Learn more!</a>
+            </div>
+        </div>
+    </div>
+    );
+};
+
+export default Card;
--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
-export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
+export const Footer = () => ( 
+	<footer className = "footer mt-auto py-3 text-center">
+    	<p> Made with < i className = "fa fa-heart text-danger"/> by {" "} 
+		<a href = "http://www.4geeksacademy.com"> 4 Geeks Academy </a> 
+		</p> 
 	</footer>
 );
