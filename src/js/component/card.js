7b035f8d4cfc8f5e849b98bc6acac087a3208d4c
agusalvs <agustina.alvarezsacra@gmail.com>
2023-01-17 20:54:51 +0000

    Starwars blog reading list v.2

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,63 +1,19 @@
 import React from "react";
 
-const Card = () => {
+const Card = (item) => {
+    
     return ( 
-        <div className="card-group row">
+        <div className="card-group">
             <div className="card">
                 <img src="..." className="card-img-top" alt="image"/>
                 <div className="card-body">
-                    <h5 className="card-title">Character</h5>
-                    <p className="card-text">Description</p>
+                    <h5 className="card-title">{item.name}</h5>
+                    <p className="card-text">{item.properties.eye_color}</p>
                     <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="#" class="btn btn-primary">Learn more!</a>
+                    <a href="#" className="btn btn-primary">Learn more!</a>
                 </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="image"/>
-                <div className="card-body">
-                    <h5 className="card-title">Character</h5>
-                    <p className="card-text">Description</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="#" class="btn btn-primary">Learn more!</a>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="image"/>
-                <div className="card-body">
-                    <h5 className="card-title">Character</h5>
-                    <p className="card-text">Description</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="#" class="btn btn-primary">Learn more!</a>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="image"/>
-                <div className="card-body">
-                    <h5 className="card-title">Character</h5>
-                    <p className="card-text">Description</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="#" class="btn btn-primary">Learn more!</a>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="image"/>
-                <div className="card-body">
-                    <h5 className="card-title">Character</h5>
-                    <p className="card-text">Description</p>
-                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                    <a href="#" class="btn btn-primary">Learn more!</a>
-                </div>
-            </div>
-            <div className="card">
-                <img src="..." className="card-img-top" alt="image"/>
-                <div className="card-body">
-                <h5 className="card-title">Character</h5>
-                <p className="card-text">Description</p>
-                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-                <a href="#" class="btn btn-primary">Learn more!</a>
             </div>
         </div>
-    </div>
     );
 };
 
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,10 +1,33 @@
-import React from "react";
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, {useState, useEffect} from "react";
 import "../../styles/home.css";
 import Card from "../component/card.js";
 
-export const Home = () => (
+export const Home = () => {
+
+	const [characters, setCharacters] = useState([]);
+    function getCharactersInfo() {
+        fetch("https://www.swapi.tech/api/people/")
+        .then(res => res.json())
+        // .then(data => console.log(data))
+        .then((data) => setCharacters(data.results));
+        // .catch(err => console.error(err))
+    }
+
+	useEffect(() => {
+		getCharactersInfo();
+	},[])
+	console.log(characters);
+
+		// return () => {
+		// 	cleanup
+		// }
+	
+
+	return(
+		<>
 	<div className="text-center mt-5">
-		<Card/>
+		{characters.map((item)=><Card name={item.name} key={item.uid}/>)}
 	</div>
-);
+	</>
+	);
+};
